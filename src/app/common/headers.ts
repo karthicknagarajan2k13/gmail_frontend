7f381e971190cdb781d8646bc804270a83d3d699
psaravanan <psaravanan11@gmail.com>
2016-12-21 12:20:20 +0530

    Add header

--- src/app/common/headers.ts
+++ src/app/common/headers.ts
@@ -1,8 +1,7 @@
 import { Headers } from '@angular/http';
 
 export const contentHeaders = new Headers();
-contentHeaders.append('Access-Control-Allow-Origin', '*');
 contentHeaders.append('Accept', 'application/json');
 contentHeaders.append('Content-Type', 'application/json');
-contentHeaders.append('Access-Control-Allow-Methods', 'POST, PUT, DELETE, GET');
 contentHeaders.append('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+contentHeaders.append('Content-Type', 'application/X-www-form-urlencoded');
